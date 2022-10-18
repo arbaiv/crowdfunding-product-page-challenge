1d0d726835ee3a941b806d00bad0a21705f7df7a
Abdur Rahaman <arboni2459@gmail.com>
2022-10-18 06:14:05 +0600

    Added modal pledge component

--- script/modal.js
+++ script/modal.js
@@ -2,9 +2,14 @@ import A11yDialog from "../modal-plugin/a11y-dialog.esm.js"
 
 const dialogContainer = document.getElementById('dialog-container');
 const mainDialog = new A11yDialog(dialogContainer);
-console.log("dsafgsd");
+
 const backProjectBtn = document.getElementById("back-project-btn");
+const modalCloseBtn = document.getElementById("modal-close-btn");
 backProjectBtn.addEventListener("click", () => {
-    console.log("clicked");
     mainDialog.show();
+    document.body.classList.add("scroll-lock");
+});
+modalCloseBtn.addEventListener("click", () => {
+    mainDialog.hide();
+    document.body.classList.remove("scroll-lock");
 });
